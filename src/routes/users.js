a84eb221bb5e39c94c4d569ba051275aae4fe95e
Ada Hutchings <adalynn.hutchings@gmail.com>
2018-08-03 17:54:45 -0700

    Checkpoint 3 : Complete

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,7 +4,9 @@ const userController = require("../controllers/userController");
 const validation = require("./validation");
 
 router.get("/users/sign_up", userController.signUp);
-
+router.get("/users/sign_in", userController.signInForm);
+router.get("/users/sign_out", userController.signOut);
+router.post("/users/sign_in", validation.validateUsersSignIn, userController.signIn);
 router.post("/users", validation.validateUsers, userController.create);
 
 module.exports = router;
--- src/routes/validation.js
+++ src/routes/validation.js
@@ -17,5 +17,23 @@ module.exports = {
         } else {
             return next();
         }
-    }
+    },
+
+    validateUsersSignIn(req, res, next) {
+        if(req.method === "POST") {
+            req.checkBody("email", "must be vaild").isEmail();
+            req.checkBody("password", "must be at least 6 characters in length").isLength({min: 6});
+        }
+
+        const errors = req.validationErrors();
+
+        if(errors) {
+            req.flash("error", errors);
+            return res.redirect(req.headers.referer);
+        } else {
+            return next ();
+        }
+    },
+
+    
 }
